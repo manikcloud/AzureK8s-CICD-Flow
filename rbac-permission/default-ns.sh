b8a07f9f7dadf812f331f07213221187095cbb72
varun <varun@>
2023-09-21 01:06:49 +0800

    adding rbac permission ad token

--- rbac-permission/default-ns.sh
+++ rbac-permission/default-ns.sh
@@ -19,14 +19,14 @@ metadata:
   name: ${SA_NAME}
 ---
 apiVersion: rbac.authorization.k8s.io/v1
-kind: RoleBinding
+kind: ClusterRoleBinding
 metadata:
   namespace: ${KUBE_NS}
-  name: ${SA_NAME}-edit-binding
+  name: ${SA_NAME}-admin-binding
 roleRef:
   apiGroup: rbac.authorization.k8s.io
   kind: ClusterRole
-  name: edit
+  name: cluster-admin
 subjects:
 - kind: ServiceAccount
   name: ${SA_NAME}
@@ -47,3 +47,7 @@ metadata:
 EOF
 fi
 TOKEN=$(kubectl get -n ${KUBE_NS} secret/${SA_SECRET} -o jsonpath='{.data.token}' | base64 -d)
+
+kubectl get -n default secret/sa-default-token -o jsonpath='{.data.token}' | base64 -d
+
+eyJhbGciOiJSUzI1NiIsImtpZCI6Im9YbE1vQ2xQeG82UEhMZzgzVTZOaGRvTngxSUVCcV9DNUFNSFFOaUhmMXcifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJkZWZhdWx0Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6InNhLWRlZmF1bHQtdG9rZW4iLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoic2EtZGVmYXVsdCIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6IjJmZGUwYjgwLWJiZGUtNGViYi04N2UyLTIwOThjOGM3Mjc5OSIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDpkZWZhdWx0OnNhLWRlZmF1bHQifQ.vo9pEpfMLu4jnaiesFvAdo-vbZNnnAyD_nhFavMgAta5rket4N9aU9IVaQWDhfbjR1izhymtdlc5K71mJFBQUL8nRRq_mja40um7CFreL8mo4kw51X2wGhIUof16JGDphC0FNJuLkYX2og4duxNxEAnijfAgb7H42OjSvfH5kiad9Vktu0jmV0X-63UFC7f1U2f8tbif8DmKfqvsSF66U9xY58SWxegTDddQ_74xTjkZ_3WJDqYf8ZWkK1SYxn3edw0S_ZOc_vt7BBWYlG5-0-s4yklrvItkVBSSivqx9x80fKufKAycOhER5gwkqT46_Y8zy-Vmpq3n0xC2XSy0XeHx1K0B9-rkUq6HNh6-eAX0woG5IGLez8WKbqyZKQv1qr5uVJjiWOSF64eClB4QqSBvGifIPXK3AyVYjGMN8VEDdBvJvirHmnsAre7kspwnNl81K0eCHXfCxkRYTdlmnFJuIEto-2tIPEL1qCc_ZwrLR_9U6I0_7dIXq-Ui-sorRpY_g4_yZvWJpDh9xxgKY5l514xGR68NqmQpccR8hBE4TEFzY6RlYUAhy2E8a7126wkefs2NKQ8qeucczIa_b0jsgPXAeVWZAlCLeukIyCK18s-5HeKo-hSVhLqhTzGx58rOOn2VMKUKaqVnEQmo98_mbiOErMquoocHRxDpm7Q
